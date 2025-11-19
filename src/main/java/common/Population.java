69fd12a7dc7110ba5cdf54ea14f7b19a1289762c
yalozairi <yalozairii@gmail.com>
2025-11-19 17:13:23 -0500

    added initial population setup and input files

--- src/main/java/common/Population.java
+++ src/main/java/common/Population.java
@@ -1,4 +1,28 @@
 package common;
 
 public class Population {
+
+  private String zipCode;
+  private int population;
+
+  public Population(String zipCode, int population) {
+    this.zipCode = zipCode;
+    this.population = population;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public int getPopulation() {
+    return population;
+  }
+
+  public void setPopulation(int population) {
+    this.population = population;
+  }
 }
